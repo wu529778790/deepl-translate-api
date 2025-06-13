ec8e140ded49a746f296c5c05c61c44f4fd16d76
wuwei <wuwei@fadada.com>
2025-06-13 11:07:02 +0800

    更新 Vite 配置，添加支持的模块格式，扩展外部依赖项，设置目标 Node 版本为 14，以提升构建兼容性和性能。

--- vite.config.js
+++ vite.config.js
@@ -6,14 +6,20 @@ export default defineConfig({
       entry: "./lib/main.js",
       name: "DeepLTranslateAPI",
       fileName: "deepl-translate-api",
+      formats: ["es", "cjs"],
     },
     rollupOptions: {
-      external: ["playwright"],
+      external: ["playwright", "child_process", "fs", "path", "readline"],
       output: {
         globals: {
           playwright: "playwright",
+          child_process: "child_process",
+          fs: "fs",
+          path: "path",
+          readline: "readline",
         },
       },
     },
+    target: "node14",
   },
 });
